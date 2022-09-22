0a7c80962406dd008a16229a72cac371f1140f9c
Delta <cebracho94@gmail.com>
2022-09-22 16:33:04 -0400

    adding images

--- src/utils/getWeatherImg.js
+++ src/utils/getWeatherImg.js
@@ -6,11 +6,16 @@ import sunny from "../assets/sunny.png"
 import cloudythunder from "../assets/cloudythunder.png"
 import mist from "../assets/mist.png"
 import location from "../assets/location.png"
+import rain from "../assets/rain.png"
+import snow from "../assets/snow.png"
+
 
 export const getWeatherImg = weather => {
     if(weather === "Clouds") return cloudy
-    if(weather === "Rain") return raincloud
+    if(weather === "Rain") return rain
     if(weather === "Clear") return moon
+    if(weather === "Snow") return snow
+    if(weather === "Mist") return mist
 
     return location
 }
